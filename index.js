f0720b70553817c164e6fca227b7ee8586cb2d33
Dev.Nah <mcthundercat99@gmail.com>
2022-04-23 21:51:02 -0400

    added Manager class and initial questions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code goe here
+const fs = require('fs');
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+
+const init = ()  => {
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'manName',
+            message: "What is the Team Manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your Employee id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'room',
+            message: 'What is your office number?'
+        },
+        {
+            type: 'confirm',
+            name: 'add',
+            message: 'Would you like to add another employee?',
+            default: false,
+        }
+    ]) .then(answers => {
+        const manager = new Manager(answers.manName, answers.id, answers.email, answers.room);
+        console.log(manager);
+    });
+};
+
+init ();
